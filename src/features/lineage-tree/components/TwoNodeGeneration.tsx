3224e48d57b25e06ea1a7a06699a58b8a02ab596
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2024-08-06 15:08:42 -0700

    removed imports

--- src/features/lineage-tree/components/TwoNodeGeneration.tsx
+++ src/features/lineage-tree/components/TwoNodeGeneration.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 import {AnimatePresence, motion} from 'framer-motion'
 import { Individual } from '../../../types';
 import ItemCardInfo from '../../../components/ItemCardInfo';
